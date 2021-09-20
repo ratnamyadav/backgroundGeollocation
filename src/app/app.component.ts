770dc718ccb56f3b56bfab287b4a8540b766794c
Ratnam Yadav <ratnam.yadav@itsacheckmate.com>
2021-09-21 01:18:37 +0530

    added background location check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {BackgroundMode} from '@ionic-native/background-mode/ngx';
+import { startOfDay, differenceInMinutes, addHours } from 'date-fns';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,48 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  bookingTable = {
+    latitude: 27.677950,
+    longitude: 76.354360,
+    checkedIn: false,
+    datetime: addHours(startOfDay(new Date()), 1),
+  };
+
+  constructor(public plt: Platform,
+              private backgroundMode: BackgroundMode,
+              private geolocation: Geolocation) {
+    this.plt.ready()
+      .then(() => {
+        this.backgroundMode.enable();
+        setInterval(() => {
+          this.checkIfValid(new Date());
+        }, 60000);
+      });
+  }
+
+  checkIfValid(time) {
+    const minutes = differenceInMinutes(time, this.bookingTable.datetime);
+    console.log('difference in minutes', minutes);
+    if(!this.bookingTable.checkedIn && (minutes == 15 || minutes == 30 || minutes == 45 || minutes == 60)) {
+      this.geolocation.getCurrentPosition().then((location) => {
+        let latitude = location.coords.latitude;
+        let longitude = location.coords.latitude;
+        const distance = this.distance(latitude, longitude, this.bookingTable.latitude, this.bookingTable.longitude);
+        console.log('distance', distance);
+        if(distance < 50) {
+          this.bookingTable.checkedIn = true;
+        }
+      });
+    }
+  }
+
+  distance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 +
+      c(lat1 * p) * c(lat2 * p) *
+      (1 - c((lon2 - lon1) * p))/2;
+
+    return 12742000 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundMode, Geolocation],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
