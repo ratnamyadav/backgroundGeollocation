d2939238a05a9a263c1dd3ef858b790c7d1c4e2c
Ratnam Yadav <ratnam.yadav@itsacheckmate.com>
2021-09-22 04:46:36 +0530

    fixing location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {BackgroundMode} from '@ionic-native/background-mode/ngx';
 import { startOfDay, differenceInMinutes, addHours } from 'date-fns';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {
+  BackgroundGeolocation,
+  BackgroundGeolocationConfig,
+  BackgroundGeolocationEvents,
+  BackgroundGeolocationResponse
+} from '@ionic-native/background-geolocation/ngx';
+import {Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +20,40 @@ export class AppComponent {
     latitude: 27.677950,
     longitude: 76.354360,
     checkedIn: false,
-    datetime: addHours(startOfDay(new Date()), 1),
+    datetime: addHours(startOfDay(new Date()), 4),
   };
+  lastLocation;
 
   constructor(public plt: Platform,
               private backgroundMode: BackgroundMode,
+              private backgroundGeolocation: BackgroundGeolocation,
               private geolocation: Geolocation) {
     this.plt.ready()
       .then(() => {
+        console.log('Starting app');
+        const config: BackgroundGeolocationConfig = {
+          desiredAccuracy: 10,
+          stationaryRadius: 20,
+          distanceFilter: 30,
+          debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+          stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+        };
         this.backgroundMode.enable();
+        this.backgroundGeolocation.configure(config)
+          .then(() => {
+
+            this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
+              this.lastLocation = location;
+              console.log('last location', JSON.stringify(location));
+              // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
+              // and the background-task may be completed.  You must do this regardless if your operations are successful or not.
+              // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+              this.backgroundGeolocation.finish(); // FOR IOS ONLY
+            });
+
+          });
+        // start recording location
+        this.backgroundGeolocation.start();
         setInterval(() => {
           this.checkIfValid(new Date());
         }, 60000);
@@ -33,15 +64,12 @@ export class AppComponent {
     const minutes = differenceInMinutes(time, this.bookingTable.datetime);
     console.log('difference in minutes', minutes);
     if(!this.bookingTable.checkedIn && (minutes == 15 || minutes == 30 || minutes == 45 || minutes == 60)) {
-      this.geolocation.getCurrentPosition().then((location) => {
-        let latitude = location.coords.latitude;
-        let longitude = location.coords.latitude;
-        const distance = this.distance(latitude, longitude, this.bookingTable.latitude, this.bookingTable.longitude);
-        console.log('distance', distance);
-        if(distance < 50) {
-          this.bookingTable.checkedIn = true;
-        }
-      });
+      const distance = this.distance(this.lastLocation.latitude, this.lastLocation.longitude,
+        this.bookingTable.latitude, this.bookingTable.longitude);
+      console.log('distance', distance);
+      if(distance < 50) {
+        this.bookingTable.checkedIn = true;
+      }
     }
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BackgroundMode } from '@ionic-native/background-mode/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {BackgroundGeolocation} from '@ionic-native/background-geolocation/ngx';
+import {Geolocation} from "@ionic-native/geolocation/ngx";
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundMode, Geolocation],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundMode, Geolocation, BackgroundGeolocation],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
